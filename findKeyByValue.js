89386927385b6eadfb8293dfe42e23dc2e4250e8
vagrant <shadmerhi@gmail.com>
2020-09-16 21:01:36 +0000

    Returns key which matches desired value

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+const findKeyByValue = function(searchObj, valueWeWant) {
+
+  let keys = Object.keys(searchObj);
+  for (let key of keys) {
+    if (searchObj[key] === valueWeWant) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Family Guy"));
