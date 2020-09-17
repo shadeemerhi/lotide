cf1b2db115340d1f37c4797ec68e30aebc58747d
vagrant <shadmerhi@gmail.com>
2020-09-17 21:49:56 +0000

    Returns the first instance of callback being true

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const findKey = (ourObject, callback) => {
+
+  let keys = Object.keys(ourObject);
+  for (let key of keys) {
+    if (callback(ourObject[key])){
+      return key;
+    }
+  }
+  return undefined;
+}
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
